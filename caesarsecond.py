37f0a66bfe9c8bba32f1edadf198f27df3ad078e
Admin <gavri840@mail.ru>
2016-10-09 13:31:50 -0400

    caesarsecond

---
+++ caesarsecond.py
@@ -0,0 +1,12 @@
+plaintext=input('Введите слово')
+def encrypt_caesar(plaintext):
+    ciphertext=''
+    for i in range(len(plaintext)): 
+        ch=ord(plaintext[i])
+        if ch<=122 and ch>=120 or ch<=90 and ch>=88:
+            ch-=23
+        else:
+            ch+=3
+        ciphertext+=chr(ch)
+    return ciphertext
+print(encrypt_caesar(plaintext))	
