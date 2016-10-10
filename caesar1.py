d57350c3e799ed30d8cf846c3edc66088980f849
Dasha2202 <Gavri840@mail.ru>
2016-10-10 13:23:53 +0200

    Update caesar1.py

--- caesar1.py
+++ caesar1.py
@@ -1,13 +1,14 @@
 ciphertext = input('Введите слово')
+
+
 def encrypt_caesar(ciphertext):
-	plaintext=''
-	for i in range(len(ciphertext)):
-		ch=ord(ciphertext[i])
-		if ch<=99 and ch>=97 or ch<=67 and ch>=65:
-			ch+=23
-		else:
-			ch-=3
-		plaintext+=chr(ch)
-	return plaintext
-print(encrypt_caesar(ciphertext))
-	
+    plaintext = ''
+    for i in range(len(ciphertext)):
+        ch = ord(ciphertext[i])
+    if ch < = 99 and ch > = 97 or ch < = 67 and ch > = 65:
+        ch += 23
+    else:
+        ch -= 3
+    plaintext + = chr(ch)
+return plaintext
+print (encrypt_caesar(ciphertext))
