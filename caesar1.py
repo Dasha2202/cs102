33762c058e98b5ad08ec436254aeba8d9bbc1e39
Dasha2202 <Gavri840@mail.ru>
2016-10-17 19:49:28 +0300

    Update caesar1.py

--- caesar1.py
+++ caesar1.py
@@ -5,10 +5,10 @@ def encrypt_caesar(ciphertext):
     plaintext = ''
     for i in range(len(ciphertext)):
         ch = ord(ciphertext[i])
-    if ch < = 99 and ch > = 97 or ch < = 67 and ch > = 65:
-        ch += 23
-    else:
-        ch -= 3
-    plaintext + = chr(ch)
-return plaintext
+        if ch < = 99 and ch > = 97 or ch < = 67 and ch > = 65:
+            ch += 23
+        else:
+            ch -= 3
+        plaintext + = chr(ch)
+    return plaintext
 print (encrypt_caesar(ciphertext))
